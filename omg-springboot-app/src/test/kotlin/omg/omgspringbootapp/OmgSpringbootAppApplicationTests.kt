2dd797f374f0364f5efbc4bad3a6eb262657915f
맹순영 <symaeng98@naver.com>
2023-03-18 16:42:18 +0900

    feat: create springboot app for omg
    - 스프링부트 앱 생성

---
+++ omg-springboot-app/src/test/kotlin/omg/omgspringbootapp/OmgSpringbootAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package omg.omgspringbootapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OmgSpringbootAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
