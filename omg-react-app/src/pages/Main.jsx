8f222fff947a9eeff661c47ecaf61271bf547c27
맹순영 <symaeng98@naver.com>
2023-04-08 17:12:41 +0900

    feat: 메인 페이지 구현
    - 새로운 버튼 컴포넌트들 적용

--- omg-react-app/src/pages/Main.jsx
+++ omg-react-app/src/pages/Main.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FontTemplate from '../components/FontTemplate';
 import Button from '../components/Button';
 import uploadIcon from '../assets/upload.svg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import UploadButton from '../components/UploadButton';
+import FontGenerateButton from '../components/FontGenerateButton';
+import axios from 'axios';
 
 const MainStyle = {
     padding: "5% 5% 0 5%"
@@ -36,7 +39,31 @@ const BottomButtonDivStyle = {
 }
 
 const Main = () => {
+    const [uploadedFile, setUploadedFile] = useState(null);
+
+    const handleFileUpload = (file) => {
+        // 파일 업로드 처리 로직
+        setUploadedFile(file);
+    };
     const arr = ["갊", "갸", "곁", "곬", "교", "높", "뉑", "닳", "무", "벚", "숱", "펾"];
+
+    const sendImageToServer = async () => {
+        const formData = new FormData();
+        formData.append('file', uploadedFile);
+
+        try {
+            const response = await axios.post('http://localhost:8080/api/v1/font/new', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            console.log(response.data); // 외부 URL로 전송된 파일의 정보 출력
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div style={MainStyle}>
             <div style={TitleDescriptionDivLayoutStyle}>
@@ -49,14 +76,14 @@ const Main = () => {
             </div>
 
             <div>
-                <Button type="upload" content="손글씨 업로드" icon={uploadIcon} />
+                <UploadButton onFileUpload={handleFileUpload} content="손글씨 업로드" icon={uploadIcon} />
             </div>
 
             <div style={BottomButtonDivStyle}>
-                <Link style={{marginRight: "auto"}} to={"/"}>
+                <Link style={{ marginRight: "auto" }} to={"/"}>
                     <Button type="prev" content="이전으로" />
                 </Link>
-                <Button type="create" content="폰트 만들기" />
+                <FontGenerateButton uploadedFile={uploadedFile} content="폰트 만들기" onFileUpload={sendImageToServer} />
             </div>
         </div>
     );
