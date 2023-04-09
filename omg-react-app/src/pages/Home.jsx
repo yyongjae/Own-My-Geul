b1b944fb4f77c44555721e4726ef42817d8b2e3c
맹순영 <symaeng98@naver.com>
2023-04-09 18:05:39 +0900

    refactor: 홈 화면 css 스타일 변경
    - object 스타일에서 module로 변경

--- omg-react-app/src/pages/Home.jsx
+++ omg-react-app/src/pages/Home.jsx
@@ -5,27 +5,16 @@ import Button from '../components/Button';
 import {Link} from 'react-router-dom';
 import styles from "../styles/Home.module.css"
 
-const OMGFontStyle = {
-    fontSize : "128px",
-    fontWeight : "600"
-}
-
-const DescriptionFontStyle = {
-    fontSize : "24px",
-    fontWeight : "400",
-    paddingLeft : "10px"
-}
-
 // TODO
 // 버튼 클릭 시 #CEC8D8로 색깔 바꾸기
 function Home(props) {
     return (
-        <div className={styles.components}>
+        <div className={styles.home}>
             <BackgroundImage url={backgroundImage}/>
             <div className={styles.description}>
                 <div>
-                    <span style={OMGFontStyle}>OMG</span>
-                    <p style={DescriptionFontStyle}>자신의 손글씨를 폰트로 만들어보세요.</p>
+                    <span className={styles.title}>OMG</span>
+                    <p className={styles.detail}>자신의 손글씨를 폰트로 만들어보세요.</p>
                         <Link to={"/main"}>
                             <Button type="big" content={"폰트 만들기"}></Button>
                         </Link>
