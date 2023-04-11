351bf57f6ddc3b49817b3b84d4418275e72d8380
맹순영 <symaeng98@naver.com>
2023-04-11 22:12:31 +0900

    feat: CORS 해결을 위한 WebConfig 추가
    - React의 포트인 3000포트를 allow 해주었음
    - 개발이 끝나면 삭제 예정

---
+++ omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/global/config/WebConfig.kt
@@ -0,0 +1,16 @@
+package omg.omgspringbootapp.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer{
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:8080", "http://localhost:3000") // 허용할 출처
+            .allowedMethods("GET", "POST") // 허용할 HTTP method
+            .allowCredentials(true) // 쿠키 인증 요청 허용
+            .maxAge(3000) // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
+}
