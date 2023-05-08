919d9799ec6dfb423c35fb668e383dffd934a38f
맹순영 <symaeng98@naver.com>
2023-05-08 17:48:12 +0900

    chore: ci/cd 구축 테스트
    - CI/CD merge 이벤트 테스트

--- omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/domain/font/controller/FontController.kt
+++ omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/domain/font/controller/FontController.kt
@@ -38,7 +38,7 @@ class FontController (private val fontService: FontService) {
         // GCP 폰트 업로드
         fontService.uploadFont(font)
 
-        // CI/CD 테스트 3
+        // CI/CD 테스트 4
 
         return ResponseEntity.ok(CommonResponse().response(true,"폰트 생성 성공"))
     }
