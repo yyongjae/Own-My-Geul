7d0252519f55ceb381e519754973dc8615758cde
맹순영 <symaeng98@naver.com>
2023-05-13 21:37:44 +0900

    chore: jpa 사용을 위한 플러그인 설치
    -

--- omg-springboot-app/build.gradle.kts
+++ omg-springboot-app/build.gradle.kts
@@ -3,8 +3,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.9"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("org.jetbrains.kotlin.plugin.jpa") version "1.3.61"
+	id("org.jetbrains.kotlin.plugin.allopen") version "1.3.61"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+}
+
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
 }
 
 group = "omg"
