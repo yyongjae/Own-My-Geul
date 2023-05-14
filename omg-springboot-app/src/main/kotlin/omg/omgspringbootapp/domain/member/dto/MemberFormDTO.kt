6e99656cfbfa4e1f92aa228b65d52394ea19eaec
맹순영 <symaeng98@naver.com>
2023-05-14 23:58:42 +0900

    feat: 회원 가입 form validation 적용
    -

--- omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/domain/member/dto/MemberFormDTO.kt
+++ omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/domain/member/dto/MemberFormDTO.kt
@@ -1,10 +1,15 @@
 package omg.omgspringbootapp.domain.member.dto
 
 import omg.omgspringbootapp.domain.member.entity.Member
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
 
 data class MemberFormDTO(
-    val email: String,
+    @field: Email(message = "이메일은 필수입니다.")
+    var email: String,
+    @field: NotBlank
     val password: String,
+    @field: NotBlank
     val name: String
 ) {
     fun toEntity(): Member {
