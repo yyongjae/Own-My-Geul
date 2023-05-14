a1e8ff867c3c519a7582b3a2d2a317fbab7f0686
맹순영 <symaeng98@naver.com>
2023-05-14 17:34:20 +0900

    fix: BaseTimeEntity 수정
    - LocalDateTime으로 수정 및 외부에서 접근 가능하게 private을 삭제하고 protected로 막음

--- omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/OmgSpringbootAppApplication.kt
+++ omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/OmgSpringbootAppApplication.kt
@@ -2,8 +2,10 @@ package omg.omgspringbootapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class OmgSpringbootAppApplication
 
 fun main(args: Array<String>) {
--- omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/global/entity/BaseTimeEntity.kt
+++ omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/global/entity/BaseTimeEntity.kt
@@ -3,7 +3,7 @@ package omg.omgspringbootapp.global.entity
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import java.sql.Timestamp
+import java.time.LocalDateTime
 import javax.persistence.Column
 import javax.persistence.EntityListeners
 import javax.persistence.MappedSuperclass
@@ -12,10 +12,11 @@ import javax.persistence.MappedSuperclass
 @EntityListeners(AuditingEntityListener::class) // JPA Entity에서 이벤트가 발생할 때마다 특정 로직을 실행
 class BaseTimeEntity {
     @CreatedDate
-    @Column(columnDefinition = "TIMESTAMP", updatable = false)
-    private val createdAt: Timestamp? = null
+    @Column(nullable = false, updatable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now()
 
     @LastModifiedDate
-    @Column(columnDefinition = "TIMESTAMP")
-    private val updatedAt: Timestamp? = null
+    @Column(nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+        protected set
 }
