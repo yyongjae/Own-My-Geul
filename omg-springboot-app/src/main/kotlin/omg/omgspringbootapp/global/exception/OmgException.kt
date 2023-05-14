5ef1d7815ce79cc1eb466e820fd7521aff8e3c10
맹순영 <symaeng98@naver.com>
2023-05-14 15:03:04 +0900

    feat: 회원 없을 때의 Exception 구현
    - NoSuchMemberException

--- omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/global/exception/OmgException.kt
+++ omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/global/exception/OmgException.kt
@@ -5,6 +5,7 @@ enum class OmgException (
     val message: String
     ){
     // 000번대 회원
+    NO_SUCH_MEMBER("BE001", "No Such Member"),
     // 100번대 폰트
     NO_SUCH_FONT("BE101", "No Such Font"),
     FAIL_FONT_CREATION("BE102","Cannot Create Font")
--- omg-springboot-app/src/test/kotlin/omg/omgspringbootapp/OmgSpringbootAppApplicationTests.kt
+++ omg-springboot-app/src/test/kotlin/omg/omgspringbootapp/OmgSpringbootAppApplicationTests.kt
@@ -1,13 +1,27 @@
 package omg.omgspringbootapp
 
+import omg.omgspringbootapp.domain.member.entity.Member
+import omg.omgspringbootapp.domain.member.repository.MemberRepository
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
-//@SpringBootTest
-class OmgSpringbootAppApplicationTests {
+@DataJpaTest
+class OmgSpringbootAppApplicationTests(
+	@Autowired val memberRepository: MemberRepository
+) {
 
 	@Test
-	fun contextLoads() {
+	fun saveMemberTest() {
+		val member = Member(
+			"symaeng98@naver.com",
+			"1234",
+			"맹순영"
+		)
+
+		val saveMember = memberRepository.save(member)
+
 	}
 
 }
