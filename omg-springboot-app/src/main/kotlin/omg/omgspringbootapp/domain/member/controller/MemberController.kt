c2e2260e75bf263f7d3f971e79ebc70906ec430b
맹순영 <symaeng98@naver.com>
2023-05-15 00:00:04 +0900

    feat: 회원가입 api 구현
    -

---
+++ omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/domain/member/controller/MemberController.kt
@@ -0,0 +1,19 @@
+package omg.omgspringbootapp.domain.member.controller
+
+import omg.omgspringbootapp.domain.member.dto.MemberFormDTO
+import omg.omgspringbootapp.domain.member.service.MemberService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/api/v1/member")
+class MemberController(
+    private val memberService: MemberService
+) {
+    @PostMapping("/join")
+    fun createMember(@Valid memberFormDTO: MemberFormDTO) {
+        memberService.join(memberFormDTO)
+    }
+}
