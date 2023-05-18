95908e0d18376d308ead3e6c3eb638e5a6a1594a
맹순영 <symaeng98@naver.com>
2023-05-18 16:01:04 +0900

    feat: 회원가입 응답 생성
    -

--- omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/domain/member/controller/MemberController.kt
+++ omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/domain/member/controller/MemberController.kt
@@ -2,6 +2,8 @@ package omg.omgspringbootapp.domain.member.controller
 
 import omg.omgspringbootapp.domain.member.dto.MemberFormDTO
 import omg.omgspringbootapp.domain.member.service.MemberService
+import omg.omgspringbootapp.global.dto.response.CommonResponse
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,7 +16,11 @@ class MemberController(
     private val memberService: MemberService
 ) {
     @PostMapping("/join")
-    fun createMember(@RequestBody @Valid memberFormDTO: MemberFormDTO) {
+    fun createMember(
+        @RequestBody @Valid memberFormDTO: MemberFormDTO
+    ): ResponseEntity<CommonResponse> {
         memberService.join(memberFormDTO)
+
+        return ResponseEntity.ok(CommonResponse().response(false, "회원 가입을 성공하였습니다."))
     }
 }
