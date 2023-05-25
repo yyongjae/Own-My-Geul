40bf0ef9b7b54bff075112086643ba7f47c4e357
맹순영 <symaeng98@naver.com>
2023-05-25 23:43:16 +0900

    chore: jwt 토큰 구현을 위한 설정
    -

--- omg-springboot-app/build.gradle.kts
+++ omg-springboot-app/build.gradle.kts
@@ -42,6 +42,9 @@ dependencies {
 	implementation ("com.h2database:h2")
 	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation ("org.springframework.boot:spring-boot-starter-validation")
+	implementation ("io.jsonwebtoken:jjwt-api:0.11.2")
+	implementation ("io.jsonwebtoken:jjwt-impl:0.11.2")
+	implementation ("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
 
 tasks.withType<KotlinCompile> {
