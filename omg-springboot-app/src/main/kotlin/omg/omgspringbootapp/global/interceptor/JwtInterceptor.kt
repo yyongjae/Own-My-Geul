1b542616a8f9bae7db296d1ea0391b4bce283f84
맹순영 <symaeng98@naver.com>
2023-05-27 12:47:44 +0900

    fix: 헤더 이름 및 변수 이름 변경
    - Access-Token -> Authorization : 헤더 이름을 어떤 토큰인지 명시하는게 보안 상으로 좋지 않다고 판단
    - accessToken -> token : 토큰의 종류를 명시하는 것 또한 보안 상 좋지 않다고 판단

--- omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/global/interceptor/JwtInterceptor.kt
+++ omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/global/interceptor/JwtInterceptor.kt
@@ -10,9 +10,9 @@ class JwtInterceptor(
 ): HandlerInterceptor {
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        val bearerToken = request.getHeader("Access-Token") ?: throw IllegalArgumentException("필수 헤더값이 없습니다.")
-        val accessToken = jwtUtil.resolveToken(bearerToken)
-        val memberId = jwtUtil.getMemberIdFromToken(accessToken)
+        val bearerToken = request.getHeader("Authorization") ?: throw IllegalArgumentException("필수 헤더값이 없습니다.")
+        val token = jwtUtil.resolveToken(bearerToken)
+        val memberId = jwtUtil.getMemberIdFromToken(token)
 
         // 추출한 멤버 ID를 요청 속성(attribute)에 저장하여 컨트롤러에서 사용할 수 있도록 합니다.
         request.setAttribute("memberId", memberId)
