1df607233e873423c479bdb16ea2e35c931bfff4
맹순영 <symaeng98@naver.com>
2023-05-27 14:33:16 +0900

    fix: 요청 수정
    - var -> val: 한 번 전달한 email 정보는 변하지 않을 것으로 판단

--- omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/domain/member/dto/request/JoinRequest.kt
+++ omg-springboot-app/src/main/kotlin/omg/omgspringbootapp/domain/member/dto/request/JoinRequest.kt
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotBlank
 data class JoinRequest(
     @field: Email(message = "이메일 형식에 맞춰서 작성했는지 확인하세요.")
     @field: NotBlank(message = "이메일은 필수 입력 항목입니다.")
-    var email: String,
+    val email: String,
     @field: NotBlank(message = "비밀번호는 필수입니다.")
     val password: String,
     @field: NotBlank(message = "이름은 필수입니다.")
