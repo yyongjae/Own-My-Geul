c520259048bff89d2490b0ea07164451d72904f1
맹순영 <symaeng98@naver.com>
2023-06-02 22:08:53 +0900

    feat: 로그인 화면 개발
    - 기존의 홈 화면 수정하여 로그인 화면으로 변경

--- omg-react-app/src/components/Button.jsx
+++ omg-react-app/src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import styles from '../styles/Button.module.css'
 
 function Button(props) {
+    const navigate = useNavigate();
+
+    const goToMain = () => {
+        navigate('/main')
+    };
 
     if (props.type === "big") {
         return (
@@ -18,6 +24,25 @@ function Button(props) {
         return (
             <button className={styles["prev"]}>
                 <span className={styles["prev-span"]}>{props.content}</span>
+            </button>
+        )
+    }
+
+    if (props.type === "login") {
+
+        return (
+            <div className={styles["login-div"]}>
+                <button onClick={goToMain} className={styles["login"]}>
+                    <span className={styles["login-span"]}>{props.content}</span>
+                </button>
+            </div>
+        )
+    }
+
+    if (props.type === "join") {
+        return (
+            <button className={styles["join"]}>
+                회원가입
             </button>
         )
     }
--- omg-react-app/src/pages/Home.jsx
+++ omg-react-app/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import backgroundImage from '../assets/homeBackgroundImage.png'
 import BackgroundImage from "../components/BackgroundImage";
 import Button from '../components/Button';
-import {Link} from 'react-router-dom';
 import styles from "../styles/Home.module.css"
 
 // TODO
@@ -13,11 +12,21 @@ function Home(props) {
             <BackgroundImage url={backgroundImage}/>
             <div className={styles.description}>
                 <div>
-                    <span className={styles.title}>OMG</span>
+                    <div className={styles.title}>OMG</div>
                     <p className={styles.detail}>자신의 손글씨를 폰트로 만들어보세요.</p>
-                        <Link to={"/main"}>
-                            <Button type="big" content={"폰트 만들기"}></Button>
-                        </Link>
+                    <div className={styles.login_input}>
+                        <div className={styles.textForm}>
+                            <input name="email" type="text" className={styles.id_input} placeholder="이메일을 입력하세요"></input>
+                        </div>
+                        <div className={styles.textForm}>
+                            <input name="password" type="password" className={styles.pw_input} placeholder="비밀번호를 입력하세요"></input>
+                        </div>
+                    </div>
+                    <Button type="login" content={"로그인"}></Button>
+                    <div className={styles.join}>
+                        <div>계정이 없으신가요?</div>
+                        <Button type="join" content={"회원가입"}></Button>
+                    </div>
                 </div>
             </div>
         </div>
