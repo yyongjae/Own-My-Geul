fe041cf80277f1d32e1ca7ff6078e67893f08220
맹순영 <symaeng98@naver.com>
2023-06-07 23:53:44 +0900

    fix: 로그인시 인사말 나오게 변경

--- omg-react-app/src/pages/Home.jsx
+++ omg-react-app/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import backgroundImage from '../assets/homeBackgroundImage.png'
 import BackgroundImage from "../components/BackgroundImage";
@@ -6,6 +6,7 @@ import Button from '../components/Button';
 import LoginButton from '../components/buttons/LoginButton';
 import styles from "../styles/Home.module.css"
 import axios from 'axios';
+import StartButton from '../components/buttons/StartButton';
 
 // TODO
 // 버튼 클릭 시 #CEC8D8로 색깔 바꾸기
@@ -16,59 +17,75 @@ function Home(props) {
         // 이메일과 비밀번호 입력값 가져오기
         const email = document.getElementsByName('email')[0].value;
         const password = document.getElementsByName('password')[0].value;
-      
+
         // 서버로 전송할 데이터 생성
         const data = {
-          email: email,
-          password: password
+            email: email,
+            password: password
         };
 
         // 서버로 POST 요청 보내기
         axios.post(backendServerUrl + '/api/v1/member/login', data)
-        .then(response => {
-            console.log(response.data);
-            // 요청이 성공한 경우 처리
-            if (response.data.status === true) {
-                const memberInfo = response.data.data.memberInfo
-                const tokenInfo = response.data.data.tokenInfo
-                const accessToken = tokenInfo.grantType + " " + tokenInfo.accessToken
+            .then(response => {
+                console.log(response.data);
+                // 요청이 성공한 경우 처리
+                if (response.data.status === true) {
+                    const memberInfo = response.data.data.memberInfo
+                    const tokenInfo = response.data.data.tokenInfo
+                    const accessToken = tokenInfo.grantType + " " + tokenInfo.accessToken
 
-                localStorage.setItem("name", memberInfo.name)
-                localStorage.setItem("accessToken", accessToken)
-                navigate('/main')
-            }
-            alert(response.data.message)
-        })
-        .catch(error => {
-            // 요청이 실패한 경우 처리
-            console.error(error);
-        });
+                    localStorage.setItem("name", memberInfo.name)
+                    localStorage.setItem("accessToken", accessToken)
+                    localStorage.setItem("isLoggedIn", true)
+                    
+                    navigate('/')
+                }
+                alert(response.data.message)
+            })
+            .catch(error => {
+                // 요청이 실패한 경우 처리
+                console.error(error);
+            });
     }
 
     return (
         <div className={styles.home}>
-            <BackgroundImage url={backgroundImage}/>
-            <div className={styles.description}>
-                <div>
-                    <div className={styles.title}>OMG</div>
-                    <p className={styles.detail}>자신의 손글씨를 폰트로 만들어보세요.</p>
-                    <div className={styles.login_input}>
-                        <div className={styles.textForm}>
-                            <input  name="email" type="email" className={styles.id_input} placeholder="이메일을 입력하세요"></input>
-                        </div>
-                        <div className={styles.textForm}>
-                            <input name="password" type="password" className={styles.pw_input} placeholder="비밀번호를 입력하세요"></input>
-                        </div>
-                    </div>
-                    <LoginButton handleLogin={handleLogin} content={"로그인"}></LoginButton>
-                    <div className={styles.join}>
-                        <div>계정이 없으신가요?</div>
-                        <Link to="/join">
-                            <Button type="join" content={"회원가입"}></Button>
-                        </Link>
+            <BackgroundImage url={backgroundImage} />
+            {localStorage.getItem("isLoggedIn") ? (
+                <div className={styles.description}>
+                    <div>
+                        <div className={styles.greeting}>
+                            안녕하세요
+                            <span style={{fontWeight:"700", color:"#637fe4"}}> {localStorage.getItem("name")} </span>
+                            님
+                         </div>
+                        <p className={styles.detail}>자신의 손글씨를 폰트로 만들어보세요.</p>
+                        <StartButton content={"폰트 만들기"}></StartButton>
                     </div>
                 </div>
-            </div>
+            ) : (
+                <div className={styles.description}>
+                    <div>
+                        <div className={styles.title}>OMG</div>
+                        <p className={styles.detail}>자신의 손글씨를 폰트로 만들어보세요.</p>
+                        <div className={styles.login_input}>
+                            <div className={styles.textForm}>
+                                <input name="email" type="email" className={styles.id_input} placeholder="이메일을 입력하세요"></input>
+                            </div>
+                            <div className={styles.textForm}>
+                                <input name="password" type="password" className={styles.pw_input} placeholder="비밀번호를 입력하세요"></input>
+                            </div>
+                        </div>
+                        <LoginButton handleLogin={handleLogin} content={"로그인"}></LoginButton>
+                        <div className={styles.join}>
+                            <div>계정이 없으신가요?</div>
+                            <Link to="/join">
+                                <Button type="join" content={"회원가입"}></Button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
